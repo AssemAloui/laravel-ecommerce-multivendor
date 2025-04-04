db363f591feb41276c5f9f759b79166f69b8c9bf
AssemAloui <assemaloui@gmail.com>
2025-04-04 21:43:46 +0100

    orders and orders items table added

--- store/database/migrations/2025_03_27_155425_create_carts_table.php
+++ store/database/migrations/2025_03_27_155425_create_carts_table.php
@@ -21,6 +21,8 @@ return new class extends Migration
             $table->unsignedSmallInteger('quantity')->default(1);
             $table->json('options')->nullable();
             $table->timestamps();
+            $table->unique(['cookie_id','product_id']);
+
         });
     }
 
--- store/resources/views/layouts/front.blade.php
+++ store/resources/views/layouts/front.blade.php
@@ -20,6 +20,7 @@
             display: block !important;
         }
     </style>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     @stack('styles')
 </head>
 
